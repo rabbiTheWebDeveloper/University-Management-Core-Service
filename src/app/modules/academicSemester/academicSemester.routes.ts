b34285f53f7dcde6bf8be48553806408a4b5112f
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-09-16 11:37:11 +0600

    update

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import { AcademicSemesterContoral } from './academicSemester.controlar';
 const router = express.Router();
 
-router.post('/', AcademicSemesterContoral.insertIntoDB);
+router.post('/add-semester', AcademicSemesterContoral.insertIntoDB);
 export const AcademicSemesterRouter = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/academicSemester',
+    path: '/academic-semesters',
     route: AcademicSemesterRouter,
   },
 ];
